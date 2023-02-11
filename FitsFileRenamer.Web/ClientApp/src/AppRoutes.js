eebf9f37c073f2db4335cbe5ddbbd1e39d6b276e
Matt Schechtman <matthew.d.schechtman@gmail.com>
2023-02-11 17:36:44 -0500

    Start adding new component

--- FitsFileRenamer.Web/ClientApp/src/AppRoutes.js
+++ FitsFileRenamer.Web/ClientApp/src/AppRoutes.js
@@ -1,11 +1,16 @@
 import { Counter } from "./components/Counter";
 import { FetchData } from "./components/FetchData";
 import { Home } from "./components/Home";
+import { Files} from "./components/Files";
 
 const AppRoutes = [
   {
     index: true,
     element: <Home />
+  },
+  {
+    path: '/files',
+    element: <Files />
   },
   {
     path: '/counter',
---
+++ FitsFileRenamer.Web/ClientApp/src/components/Files.js
@@ -0,0 +1,57 @@
+﻿import React, {Component} from 'react';
+
+export class Files extends Component {
+    static displayName = Files.name;
+
+    constructor(props) {
+        super(props);
+        this.state = {files: [], path: '', loading: true};
+        this.handleSelect = this.handleSelect.bind(this);
+    }
+
+    handleSelect(event) {
+        console.log(event);
+        //this.populateFilesData();
+    }
+
+    static renderFiles(files) {
+        return (
+            <table className="table table-striped" aria-labelledby="tableLabel">
+                <thead>
+                <tr>
+                    <th>FullPathName</th>
+                    <th>Date</th>
+                </tr>
+                </thead>
+                <tbody>
+                {files.map(file =>
+                    <tr key={file.FullPathName}>
+                        <td>{file.FullPathName}</td>
+                        <td>{file.Date}</td>
+                    </tr>
+                )}
+                </tbody>
+            </table>
+        );
+    }
+
+    render() {
+        let contents = this.state.loading
+            ? <p><em>Loading...</em></p>
+            : Files.renderFiles(this.state.files);
+
+        return (
+            <div>
+                <h1 id="tableLabel">Files</h1>
+                <input directory="" webkitdirectory="" mozdirectory="" type="file" onChange={this.handleSelect} />
+                {contents}
+            </div>
+        );
+    }
+
+    async populateFilesData() {
+        const response = await fetch(`files?path=${this.state.path}`);
+        const data = await response.json();
+        this.setState({forecasts: data, loading: false});
+    }
+}
